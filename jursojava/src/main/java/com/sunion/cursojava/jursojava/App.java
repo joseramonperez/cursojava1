2c10b3fb7fc3f320d371ef3493e9fcb3efd1bf82
joseramonperez <39797585+joseramonperez@users.noreply.github.com>
2018-05-31 13:30:15 +0200

    JOSE RAMON

---
+++ jursojava/src/main/java/com/sunion/cursojava/jursojava/App.java
@@ -0,0 +1,13 @@
+package com.sunion.cursojava.jursojava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
